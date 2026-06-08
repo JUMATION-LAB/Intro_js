a0474089e85ebcc915205032292a572ff6f4f167
JUMATION-LAB <leopoldmatovu9@gmail.com>
2026-06-08 17:52:17 +0300

    My first Git Commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = "Matovu Juma Kikonyogo";
+let age = 23;
+let favouriteProgrammingLanguage = "JavaScript";
+
+console.log(`Hello, my name is ${name}. I am ${age} years old and my favourite programming language is ${favouriteProgrammingLanguage}.`);
